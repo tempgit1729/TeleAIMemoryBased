281a6cefea4c41fc84c7b7f66e617df10dccf93c
tempgit1729 <tempgithub@founder.work.gd>
2025-05-02 10:26:07 +0000

    update

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 # Load environment variables from .env file
 load_dotenv()
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-GEMENI_API_KEY = os.getenv("GEMENI_API_KEY")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN") #Telegram API
+GEMENI_API_KEY = os.getenv("GEMENI_API_KEY") #Gemini API
 DB_PATH = "chat_history.db"
 MAX_TOKENS = 1048576  # Trim if history exceeds this
